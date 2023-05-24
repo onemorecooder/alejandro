94202481282020467fa9cd6848659f7225114261
Alejandro <ajosetorrespiedra97@gmail.com>
2023-05-24 22:07:37 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SplineScene from "./components/SplineScene";
+
+function App() {
+  return (
+    <div className="h-screen">
+      <SplineScene />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SplineScene.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+const SplineScene = () => {
+  return (
+    <Spline
+      className="h-10"
+      scene="https://prod.spline.design/9dTjW8kozdkmv1jo/scene.splinecode"
+    />
+  );
+};
+
+export default SplineScene;
